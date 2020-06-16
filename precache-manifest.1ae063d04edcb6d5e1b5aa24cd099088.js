3fde08b766718931533e067bf05ece0ebfc27807
Aleksandra Tkachuk <lesyalesya9876@gmail.com>
2020-06-16 12:46:42 +0300

    Updates

---
+++ precache-manifest.1ae063d04edcb6d5e1b5aa24cd099088.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de0b83784c8d628e23e87784263be16c",
+    "url": "/goit-react-hw-02-phonebook/index.html"
+  },
+  {
+    "revision": "994ea8674d89fa352c67",
+    "url": "/goit-react-hw-02-phonebook/static/css/2.d34346ea.chunk.css"
+  },
+  {
+    "revision": "825d98c62b9bf7617d3d",
+    "url": "/goit-react-hw-02-phonebook/static/css/main.1cfb4f63.chunk.css"
+  },
+  {
+    "revision": "994ea8674d89fa352c67",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.c6afb2bf.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.c6afb2bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "825d98c62b9bf7617d3d",
+    "url": "/goit-react-hw-02-phonebook/static/js/main.150d92f9.chunk.js"
+  },
+  {
+    "revision": "de5dfbc78302bed1e7de",
+    "url": "/goit-react-hw-02-phonebook/static/js/runtime-main.3cc9cabd.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/goit-react-hw-02-phonebook/precache-manifest.9d4c0929fa5af02502b513181a4fdf91.js"
+  "/goit-react-hw-02-phonebook/precache-manifest.1ae063d04edcb6d5e1b5aa24cd099088.js"
 );
 
 self.addEventListener('message', (event) => {
